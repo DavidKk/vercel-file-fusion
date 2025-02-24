dda1b242e41ae734f0a4a955adb59135fcafe5ad
DavidJones <qowera@gmail.com>
2025-02-25 01:04:09 +0800

    perf: nav

---
+++ app/Nav/constants.ts
@@ -0,0 +1,14 @@
+export const DEFAULT_TITLE = 'File Fusion'
+
+export const DEFAULT_NAV = {
+  compress: [
+    { name: 'Zip', href: '/zip' },
+    { name: "Unzip", href: '/unzip' },
+  ],
+  pdf: [
+    { name: 'Invoice Optimizer', href: '/invoice' },
+  ],
+  media: [
+    { name: 'Audio Embedder', href: '/audio' },
+  ],
+}
