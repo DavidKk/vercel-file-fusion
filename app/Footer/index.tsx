b8df1046deaf106137386a662e83214cd97a6cf9
DavidJones <qowera@gmail.com>
2025-03-05 11:09:26 +0800

    fix: timezone

--- app/Footer/index.tsx
+++ app/Footer/index.tsx
@@ -1,9 +1,15 @@
+'use client'
+
+import { useClient } from '@/hooks/useClient'
+
 export default function Footer() {
+  const isClient = useClient()
+
   return (
     <footer className="mt-auto py-6 bg-gradient-to-r from-gray-50 to-gray-100 shadow-inner">
       <div className="mx-auto flex justify-end items-center px-6">
         <div className="text-xs font-medium text-gray-600 tracking-wide">
-          Build Time: <span className="font-mono text-gray-700">{new Date(process.env.BUILD_TIME).toLocaleString()}</span>
+          {isClient ? `Build Time: ${(<span className="font-mono text-gray-700">{new Date(process.env.BUILD_TIME).toLocaleString()}</span>)}` : null}
         </div>
       </div>
     </footer>
